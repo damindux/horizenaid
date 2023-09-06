31c17d2f0ee775c8ca7fd56dc668d360ea23c6bb
Damindu Sandeepa <damindusandeepa2002@gmail.com>
2023-09-06 21:51:10 +0530

    pushed before switching to debian

--- components/ApiService.tsx
+++ components/ApiService.tsx
@@ -1,37 +1,54 @@
 import axios from 'axios';
 
+interface ProjectImageData {
+    id: number;
+}
+
+interface ProjectData {
+    title: string;
+    startDate: string;
+    endDate: string;
+    fundingGoal: string;
+    description: string;
+}
+
 const ApiService = {
-  createProjectImage: async (images, projectId) => {
-    const formData = new FormData();
-    images.forEach((image, index) => {
-      formData.append(`image${index + 1}`, image);
-    });
+    createProjectImage: async (images: File[], projectId: number): Promise<ProjectImageData> => {
+        const formData = new FormData();
+        images.forEach((image, index) => {
+            formData.append(`image${index + 1}`, image);
+        });
 
-    try {
-      const response = await axios.post(`http://localhost:8080/api/project-images?projectId=${projectId}`, formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-      return response.data;
-    } catch (error) {
-      throw error;
-    }
-  },
+        try {
+            const response = await axios.post<ProjectImageData>(
+                `http://localhost:8080/api/project-images?projectId=${projectId}`,
+                formData,
+                {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                }
+            );
+            return response.data;
+        } catch (error) {
+            throw error;
+        }
+    },
 
-  createProjectAndGetId: async (projectData) => {
-    try {
-      const response = await axios.post('http://localhost:8080/api/projects', projectData);
-      return response.data.id;
-    } catch (error) {
-      throw error;
-    }
-  },
+    createProjectAndGetId: async (projectData: ProjectData): Promise<number> => {
+        try {
+            const response = await axios.post<{ id: number }>('http://localhost:8080/api/projects', projectData);
+            return response.data.id;
+        } catch (error) {
+            throw error;
+        }
+    },
 
-  // Other methods...
+    // Other methods...
 };
 
 export default ApiService;
+
 
 
 
